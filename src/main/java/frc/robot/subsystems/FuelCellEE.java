6acb33e33f0e764435074847a5afaf012d33569f
KnickKnaack <55469366+KnickKnaack@users.noreply.github.com>
2020-02-06 17:48:38 -0700

    Changed Constants to Main Bot ports

--- src/main/java/frc/robot/subsystems/FuelCellEE.java
+++ src/main/java/frc/robot/subsystems/FuelCellEE.java
@@ -13,6 +13,7 @@ import frc.robot.Constants.driveConstants;
 import edu.wpi.first.wpilibj.Solenoid;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;;
 
 //import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -32,7 +33,7 @@ public class FuelCellEE extends SubsystemBase {
     fuelCellEEMot.setInverted(fuelCellEEConstants.invertFuelCellEEMot);
     fuelCellEEMot.set(ControlMode.PercentOutput, speed);
   
-    //SmartDashboard.putNumber("FuelCEllEEMot", speed);
+    SmartDashboard.putNumber("FuelCEllEEMot", speed);
   }
 
   public void fuelCellEEFlip(boolean isExtended) {
