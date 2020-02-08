367c37d91b792a3677049959460c92e29653fd51
KnickKnaack <55469366+KnickKnaack@users.noreply.github.com>
2020-02-08 10:51:43 -0700

    easier turning

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -36,7 +36,7 @@ public class Drive extends CommandBase {
     double turn = RobotContainer.m_DriverStick.getRawAxis(joyStickConstants.x_Axis);
     double scalar = RobotContainer.m_DriverStick.getRawAxis(joyStickConstants.slider);
 
-    turn = (Math.abs(turn) <= 0.25) ? 0 : turn; 
+    turn = (Math.abs(turn) <= 0.1) ? 0 : turn; 
     forward = (Math.abs(forward) <= 0.1) ? 0 : forward; 
     
     double leftMSpeed = ((-scalar*forward) - turn);
